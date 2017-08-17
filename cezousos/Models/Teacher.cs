a36c23c0586cfb18d7edfb3ae8dfc000ba381eaf
eskala <eskala@9bits.pl>
2017-08-17 18:39:05 +0200

    2nd

--- cezousos/Models/Teacher.cs
+++ cezousos/Models/Teacher.cs
@@ -13,6 +13,7 @@ namespace ProjektMVC.Models
         public string Nazwisko { get; set; }
         public string Email { get; set; }
         //
+        //
         public virtual ICollection<Classroom> Classrooms { get; set; }
         public virtual ICollection<Subject_teacher> Subject_teachers { get; set; }
     }
